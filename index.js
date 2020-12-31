64a2245ee58d8defca5a57ce38e07bb16716ab33
aditya-kharb <kharbaditya123@gmail.com>
2020-12-31 17:20:21 +0530

    initial-commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 console.log("hello this is console do anything it can do!!!")
 console.log("yahee aata hai bhai!!!")
+for (let $x = 0; $x < 5; $x++) {
+    console.log("YE BHI AATA HAI BHAI")
+    console.log("MEIN GAREEB HU!!!")
+}
+function func_1($kuch_bhe_naam) {
+    console.log($kuch_bhe_naam)
+}
+// calling the function
+$name_of_the_human = "this is is the name"
+func_1($name_of_the_human)
+class $employee {
+    salary = 10000
+    dest = "Office cubicle"
+}
+const $employee_1 = $employee
+console.log($employee_1.dest)
+console.log($employee_1.salary)
